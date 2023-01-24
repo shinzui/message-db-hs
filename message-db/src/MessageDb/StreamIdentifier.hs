84b9dd108ed6fa905e84123a8fed633190c70ee5
Nadeem Bitar <nadeem@gmail.com>
2023-01-23 20:46:37 -0800

    🍬 Add StreamIdentifier smart constructors

--- message-db/src/MessageDb/StreamIdentifier.hs
+++ message-db/src/MessageDb/StreamIdentifier.hs
@@ -1,12 +1,28 @@
-module MessageDb.StreamIdentifier (StreamIdentifier, fromText, toText) where
+module MessageDb.StreamIdentifier
+  ( StreamIdentifier,
+    fromText,
+    toText,
+    fromNatural,
+    fromUUID,
+  )
+where
 
-import Data.Text (Text, null)
+import Data.Text (Text, null, pack)
+import Data.UUID (UUID)
+import Data.UUID qualified as UUID
 import GHC.Generics (Generic)
+import Numeric.Natural (Natural)
 import Prelude hiding (null)
 
 newtype StreamIdentifier = StreamIdentifier {unIdentifier :: Text}
   deriving newtype (Eq)
   deriving stock (Ord, Show, Generic)
+
+fromNatural :: Natural -> StreamIdentifier
+fromNatural n = StreamIdentifier $ pack (show n)
+
+fromUUID :: UUID -> StreamIdentifier
+fromUUID u = StreamIdentifier $ UUID.toText u
 
 toText :: StreamIdentifier -> Text
 toText = unIdentifier
