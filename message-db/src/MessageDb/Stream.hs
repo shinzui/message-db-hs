2499b4d3b3179b7285f06f0e7ed98622b4a177a7
Nadeem Bitar <nadeem@gmail.com>
2023-01-23 19:45:20 -0800

    🎭 Refactor Identifier

--- message-db/src/MessageDb/Stream.hs
+++ message-db/src/MessageDb/Stream.hs
@@ -10,6 +10,7 @@ import Data.Generics.Labels ()
 import Data.Text (Text, intercalate, split)
 import GHC.Generics (Generic)
 import MessageDb.StreamCategory qualified as C
+import MessageDb.StreamIdentifier qualified as I
 
 newtype Identifier = Identifier {unIdentifier :: Text}
   deriving newtype (Eq)
@@ -17,18 +18,18 @@ newtype Identifier = Identifier {unIdentifier :: Text}
 
 data Stream = Stream
   { category :: !C.StreamCategory,
-    identifier :: !Identifier
+    identifier :: !I.StreamIdentifier
   }
   deriving stock (Eq, Ord, Show, Generic)
 
-mkStream :: C.StreamCategory -> Identifier -> Stream
-mkStream c i = Stream {category = c, identifier = i}
-
 toText :: Stream -> Text
-toText (Stream c i) = C.toText c <> "-" <> unIdentifier i
+toText (Stream c i) = C.toText c <> "-" <> I.toText i
 
 parse :: Text -> Either Text Stream
 parse t = case split (== '-') t of
   [] -> Left $ "Invalid stream name: " <> t
   [_c] -> Left "Invalid stream. Streams must have a '-' to separate the stream category from the stream identifier"
-  (c : i) -> C.fromText c >>= \c' -> Right $ Stream c' (Identifier $ intercalate "-" i)
+  (c : i) ->
+    C.fromText c >>= \c' ->
+      I.fromText (intercalate "-" i) >>= \i' ->
+        Right $ Stream c' i'
--- message-db/src/MessageDb/StreamIdentifier.hs
+++ message-db/src/MessageDb/StreamIdentifier.hs
@@ -1,1 +1,17 @@
-module MessageDb.StreamIdentifier where
+module MessageDb.StreamIdentifier (StreamIdentifier, fromText, toText) where
+
+import Data.Text (Text, null)
+import GHC.Generics (Generic)
+import Prelude hiding (null)
+
+newtype StreamIdentifier = StreamIdentifier {unIdentifier :: Text}
+  deriving newtype (Eq)
+  deriving stock (Ord, Show, Generic)
+
+toText :: StreamIdentifier -> Text
+toText = unIdentifier
+
+fromText :: Text -> Either Text StreamIdentifier
+fromText t
+  | null t = Left "Identifier name must at least be 1 character long"
+  | otherwise = Right $ StreamIdentifier t
